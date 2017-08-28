9c165e31411f220aca8e28a09ac86fa0e49a2a4a
JpnLavender <irimamekun@gmail.com>
2017-08-28 16:40:45 +0900

    Setting validate

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,10 @@
 class Hotel < ApplicationRecord
   has_many :user_hotels
   has_many :users, through: :user_hotels
+
+  validates :name,    length: { maximum: 20 }
+  validates :summary, presence: true
+
   class << self
     def search(word)
       self.where("name like '%#{word}%'")
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
   enum authority: {normal: 0, administrator: 1, semi_administrator: 2}
   enum sex: {men: 0, women: 1}
 
+  validates :email,      presence: true, 
+    uniqueness: true, 
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :first_name, presence: true
+  validates :last_name,  presence: true
+  validates :sex,        presence: true
+  validates :birthday,   presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :confirmable
+    :recoverable, :rememberable, :trackable, :validatable,
+    :confirmable, :confirmable
 end
